3e7fe67ade1b74833ba4e7e3b809b1bac1df2cf2
Duc Tong <tongnd@alphasoftware.vn>
2024-06-23 22:03:16 +0700

    2000

--- resources/views/package/edit.blade.php
+++ resources/views/package/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="card mb-4">
 
             <div class="card-body px-0 pt-0 pb-2 m-2">
-                <form action="{{ route('packages.store') }}" method="POST">
+                <form action="{{ route('packages.update') }}" method="POST">
                     @csrf
 
                     <div class="mb-3">
